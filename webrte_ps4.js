504c38e6e3bef09eb2f455845494ec8c9a133dbb
ps4macedo <69283643+ps4macedo@users.noreply.github.com>
2021-02-01 21:13:23 -0300

    Add files via upload

--- webrte_ps4.js
+++ webrte_ps4.js
@@ -419,8 +419,6 @@ function dumpTargetObj() {
 function findTargetObj() {
 	for (let i = 0; i < g_arr_ab_1.length; i++) {
 		if (!Int64.fromDouble(g_arr_ab_1[i][2]).equals(Int64.Zero)) {
-			debug_log("P r o c e s s a n d o . . . ");
-			debug_log("");		
 			if (g_round === 2) {
 				g_timer_leak = Int64.fromDouble(g_arr_ab_1[i][2]);
 				g_message_heading_leak = Int64.fromDouble(g_arr_ab_1[i][4]);
@@ -470,6 +468,8 @@ function sprayHTMLTextArea() {
 	debug_log("");
 	debug_log("I n i c i a n d o . . .");
 	debug_log("");
+	debug_log("P r o c e s s a n d o . . . ");
+	debug_log("");		
 
 	let textarea_div_elem = document.createElement("div");
 	document.body.appendChild(textarea_div_elem);
